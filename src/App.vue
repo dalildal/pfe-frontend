78408802c27179b0897605e7b92adf6424e76165
Dalil <dalil.rachik@student.vinci.be>
2021-12-09 10:49:41 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <v-app id="inspire">
+      <!-- Hardcodé en attendant -->
+      <sidebar/>
+      <v-content>
+        <v-container fluid>
+            <v-row class="fill-height">
+                <v-col>
+                    <transition name="fade">
+                        <router-view></router-view>
+                    </transition>
+                </v-col>
+            </v-row>
+        </v-container>
+      </v-content>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import Sidebar from '@/components/Sidebar'
+
+export default {
+  name: 'App',
+  components: {
+    Sidebar
+  }
+  
+};
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,59 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Fonction from '../views/Fonction.vue'
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
+import Announces from '../views/Announces.vue'
+import ProductDetail from '../views/ProductDetail.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/product',
+    name: 'ProductDetail',
+    component: ProductDetail
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/fonction',
+    name: 'Fonction',
+    component: Fonction
+  },
+  {
+    path: '/register',
+    name: 'Signup',
+    component: Register
+  },
+  {
+    path: '/login',
+    name: 'Signup',
+    component: Login
+  },
+  {
+    path: '/announces',
+    name: 'Announces',
+    component: Announces
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
